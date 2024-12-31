bf70cfa7bdd2289a1d2155c4b3bb3f4f87f75b01
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2024-12-31 10:17:11 +0200

    fix: ensure view registry is initialized

--- src/meter_provider_configurator.cpp
+++ src/meter_provider_configurator.cpp
@@ -7,6 +7,7 @@
 #include <opentelemetry/sdk/metrics/meter_provider_factory.h>
 #include <opentelemetry/sdk/metrics/metric_reader.h>
 #include <opentelemetry/sdk/metrics/view/view_registry.h>
+#include <opentelemetry/sdk/metrics/view/view_registry_factory.h>
 #include <opentelemetry/sdk/resource/resource.h>
 
 #include "configurator_p.h"
@@ -25,8 +26,10 @@ meter_provider_t configure_meter_provider(meter_provider_config_t&& opts)
                         ? configure_resource(std::get<resource_config_t>(opts.resource))
                         : std::get<::opentelemetry::sdk::resource::Resource>(opts.resource);
 
-    auto provider =
-        ::opentelemetry::sdk::metrics::MeterProviderFactory::Create(std::move(opts.view_registry), resource);
+    auto view_registry = opts.view_registry != nullptr ? std::move(opts.view_registry)
+                                                       : ::opentelemetry::sdk::metrics::ViewRegistryFactory::Create();
+
+    auto provider = ::opentelemetry::sdk::metrics::MeterProviderFactory::Create(std::move(view_registry), resource);
 
     for (auto&& exporter : exporters) {
         provider->AddMetricReader(get_periodic_exporting_metric_reader(std::move(exporter)));
